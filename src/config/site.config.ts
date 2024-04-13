b905908e6a664e9cbb3be4e008fef42ba065c8d6
Guy Alexis <alexis.tambie@gmail.com>
2024-04-13 16:18:51 +0000

    NextJS 14 version

---
+++ src/config/site.config.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+	title: 'QR Code Component',
+	description: 'Frontend mentor challenge',
+	logo: {
+		src: '/next.svg',
+		alt: 'Next App Logo',
+	},
+	icon: {
+		src: '/images/favicon-32x32.png',
+	},
+};
